6e2645d1d60dc45220890a1da8eb39bbe8ac56d7
kwzrd <kwzrd@pythondiscord.com>
2020-08-23 13:11:55 +0200

    Tests: add tests for resource file handling

--- tests/test_implementation.py
+++ tests/test_implementation.py
@@ -1,6 +1,27 @@
 import unittest
 
+from pypopulation import implementation as imp
+
 
 class TestImplementation(unittest.TestCase):
     """Test the implementation module."""
-    ...
+
+    # region: resource file
+
+    def test_file_exists(self):
+        """Resource file exists and lives where expected."""
+        self.assertTrue(imp.DATAFILE.exists())
+        self.assertTrue(imp.DATAFILE.is_file())
+
+    def test_file_loads(self):
+        """Resource file is valid JSON and loads into a Python list."""
+        obj = imp._load_file()
+        self.assertIsInstance(obj, list)
+
+    def test_file_is_cached(self):
+        """Resource file is cached after first load."""
+        obj_a = imp._load_file()
+        obj_b = imp._load_file()
+        self.assertIs(obj_a, obj_b)
+
+    # endregion
